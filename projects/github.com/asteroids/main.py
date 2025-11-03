986ce435f494bfac5eee3d3375516d8011913199
CaynebombYT <caynebomb@gmail.com>
2025-11-03 16:27:05 -0500

    player_create

--- projects/github.com/asteroids/main.py
+++ projects/github.com/asteroids/main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     fpsClock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_x_start = (SCREEN_WIDTH / 2)
+    player_y_start = (SCREEN_HEIGHT / 2)
+    player = Player(player_x_start, player_y_start)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = ((fpsClock.tick(60))/1000)
 
